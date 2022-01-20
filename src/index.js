d22fde133bab3fb63c2a080de71d2f2524d13d78
david-lafontant <lafontant.david@gmail.com>
2022-01-19 19:23:57 -0600

    Fix errors

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import _ from 'lodash';
-import './style.css';
+import './index.css';
 
-const taskList = document.querySelector(".list");
-let arr = [];
-let task = [
+const taskList = document.querySelector('.list');
+const arr = [];
+const task = [
   {
     id: 0,
-    description: "Wash the dish",
+    description: 'Wash the dish',
     completed: false,
   },
   {
     id: 1,
-    description: "Complete the to do list",
+    description: 'Complete the to do list',
     completed: false,
   },
   {
     id: 2,
-    description: "Take a break",
+    description: 'Take a break',
     completed: false,
   },
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   task.forEach((elem, num) => {
     arr.push(elem);
-    const listItem = document.createElement("li");
-    listItem.setAttribute("class", "item");
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class', 'item');
     listItem.innerHTML = `<i class="far fa-square" id=${task[num].id}></i>
     <p class='description'>${task[num].description}</p>
     <i class="fas fa-ellipsis-v"></i>`;
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-    title: 'Development',
-    template: './src/index.html'
+      title: 'Development',
+      template: './src/index.html',
     }),
-    ],
+  ],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
