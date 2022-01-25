6de5ed03b2da7a8ed9d85a6046fd201301c886a1
david-lafontant <lafontant.david@gmail.com>
2022-01-25 10:19:36 -0600

    Update the index

--- src/script/removeCompleted.js
+++ src/script/removeCompleted.js
@@ -1,7 +1,9 @@
 export default function taskCompleted() {
   let tasks = JSON.parse(localStorage.getItem('tasks'));
   const arr1 = tasks.filter((el) => el.completed === false);
-  console.log(arr1);
   tasks = arr1;
+  tasks.forEach((element, index) => {
+    element.id = index + 1;
+  });
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
